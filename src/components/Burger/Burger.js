4080851034cbe8ad5a181312f351e2e872173d5a
Mike Choi <mike.choi@live.ca>
2018-11-01 00:44:55 -0700

    Added flatMap method to burger component to flatten received arrays, and show message if no ingredients specified

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,33 @@ import bem from './Burger.module.scss';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
-  .map(igKey => {
+  // let transformedIngredients = [];
+  // Object.keys(props.ingredients).forEach(ingr => {
+  //   for(let i = 0; i < props.ingredients[ingr]; i++) {
+  //     transformedIngredients.push(<BurgerIngredient key={ingr+i} type={ingr} />)
+  //   }
+  // });
+
+  // let transformedIngredients = Object.keys(props.ingredients)
+  // .map(igKey => {
+  //   return [...Array(props.ingredients[igKey])].map((_, i) => {
+  //     return <BurgerIngredient key={igKey + i} type={igKey} />;
+  //   });
+  // })
+  // .reduce((arr, el) => {
+  //   return arr.concat(el)
+  // });
+
+  let transformedIngredients = Object.keys(props.ingredients)
+  .flatMap(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
       return <BurgerIngredient key={igKey + i} type={igKey} />;
     });
   });
+
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return ( 
     <div className={bem.Burger}>
