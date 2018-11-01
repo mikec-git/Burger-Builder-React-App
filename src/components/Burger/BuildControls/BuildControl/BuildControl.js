ea98cd710860d81eb46696179bd77f989cb8dd48
Mike Choi <mike.choi@live.ca>
2018-11-01 02:23:23 -0700

    Added: addedIngredient/removedIngredient event handlers and linked to build controller. Also added validation for 0 value ingredients

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,9 +3,17 @@ import c from './BuildControl.module.scss';
 
 const buildControl = (props) => (
   <div className={c.BldCtrl}>
-    <div className={c.BldCtrl__Label}>{props.label}</div>
-    <button className={c.BldCtrl__Btn +' '+ c.BldCtrl__Btn_less}>Less</button>
-    <button className={c.BldCtrl__Btn +' '+ c.BldCtrl__Btn_more}>More</button>
+    <div 
+      className={c.BldCtrl__Label}>{props.label}</div>
+    <button 
+      className={c.BldCtrl__Btn +' '+ c.BldCtrl__Btn_less}
+      onClick={props.removed}
+      disabled={props.disabled}>
+      Less</button>
+    <button 
+      className={c.BldCtrl__Btn +' '+ c.BldCtrl__Btn_more}
+      onClick={props.added}>
+      More</button>
   </div>
 );
  
